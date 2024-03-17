b4ef46aecc99c5a8888ca8869e7be7729a31ea03
alex <alex17_sv@hotmail.com>
2024-03-17 00:28:26 -0300

    corrigindo erro do eslint

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -3,9 +3,9 @@
     <q-form class="row justify-center">
       <p class="col-12 text-h5 text-center">Register</p>
       <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-md">
-        <q-input label="Name" v-model="form.name" />
-        <q-input label="Email" v-model="form.email" />
-        <q-input label="Password" v-model="form.password" />
+        <q-input v-model="form.name" label="Name"/>
+        <q-input v-model="form.email" label="Email" />
+        <q-input v-model="form.password" label="Password" />
 
         <div class="full-width q-pt-md q-gutter-y-sm">
           <q-btn
